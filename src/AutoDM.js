d0ca39f081b4185ade4dd57af55728d90ff7a855
Mark <mark.t.emery@gmail.com>
2018-06-29 12:56:35 -0700

    Changed the Greeting to bit.ly/Gamingnews

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,7 +41,7 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
+  return `What's up, ${name}! Appreciate the follow 🤙 As a heads up, just dropped a gaming news video earlier! Head to bit.ly/Gamingnews for some hilarious and informative videos, they're sort of a style mix between Philip DeFranco and PewDiePie, if you have any updates for me you can also head to my subreddit and post video game stories 😊 `; // your message
   // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
 };
 
